7e5c1f15ad1978002ff0d809e7b8a25181bd40d2
Aswath1999 <csk.aswath@gmail.com>
2023-05-19 21:28:16 +0200

    getting patient names

--- config/connection.py
+++ config/connection.py
@@ -1,19 +1,20 @@
 import psycopg2
 from psycopg2 import Error
 from decouple import config
-
+from uuid import uuid4
 
 class DatabaseManager:
     def __init__(self):
         self.conn = self.connect_database()
-
+    
+    #Connect to the database
     def connect_database(self):
         try:    
             host = 'balarama.db.elephantsql.com'
             database = config("DATABASE")
-            user = config("user")
+            user = config("user")  #USER for codespaces
             password = config("PASSWORD")
-            
+
             # Establish the connection
             conn = psycopg2.connect(
                 host=host,
@@ -26,19 +27,20 @@ class DatabaseManager:
         except Error as e:
             print(e)
 
+    #Create tables if it does not exist
     def create_tables(self):
         try:
             cursor = self.conn.cursor()
             create_table_query = '''
             CREATE TABLE IF NOT EXISTS Vitals (
-                id SERIAL PRIMARY KEY,
+                id UUID PRIMARY KEY,
                 vitals JSONB
             )
             '''
             cursor.execute(create_table_query)
             create_table_query = '''
             CREATE TABLE IF NOT EXISTS Patient (
-                id SERIAL PRIMARY KEY,
+                id UUID PRIMARY KEY,
                 patient JSONB
             )
             '''
@@ -47,7 +49,19 @@ class DatabaseManager:
             print("Created tables")
         except Error as e:
             print(e)
+    
+    #Delete tables if necessary
+    def delete_table(self,table_name):
+        try:
+            cursor = self.conn.cursor()
+            drop_table_query = f"DROP TABLE IF EXISTS {table_name};"
+            cursor.execute(drop_table_query)
+            self.conn.commit()
+            print(f"The table Patient has been deleted successfully.")
+        except Error as e:
+            print(e)
 
+    #Close the connection
     def close_connection(self):
         if self.conn:
             self.conn.close()
@@ -56,4 +70,5 @@ class DatabaseManager:
 # Usage example:
 db_manager = DatabaseManager()
 db_manager.create_tables()
+# db_manager.delete_table('Patient')
 db_manager.close_connection()
--- routes/patientpostroutes.py
+++ routes/patientpostroutes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from Models.models import Patient,DateEncoder
 from config.connection import DatabaseManager
 import json
+from uuid import uuid4
 
 
 patient= APIRouter()
@@ -12,10 +13,11 @@ async def create_patient(patient: Patient):
     try:
         connection=DatabaseManager()
         with connection.conn.cursor() as cursor:
+            primary_key_uuid = str(uuid4())
+            patient.id = primary_key_uuid
             patient_json = json.dumps(patient.dict(by_alias=True), cls=DateEncoder)
-            cursor.execute(
-            "INSERT INTO Patient (patient) VALUES (%s)",
-            (patient_json,))
+            insert_query = "INSERT INTO Patient (id, patient) VALUES (%s, %s)"
+            cursor.execute(insert_query, (primary_key_uuid, patient_json))
             connection.conn.commit()
             print("success")
     except Exception as e:
@@ -23,15 +25,37 @@ async def create_patient(patient: Patient):
         return e
     finally:
         if connection:
-            print(patient_json)
             connection.close_connection()
             return "Connection closed"
     
 
 
-@patient.get("/")
-async def get_patient():
-    return 'hi'
+@patient.get("/patient")
+async def get_all_patient():
+    try:
+        connection=DatabaseManager()
+        with connection.conn.cursor() as cursor:
+            query = "SELECT patient->'name' FROM Patient"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            patients=[]
+            for row in result:
+                patient_name_json = json.dumps(row)
+                print(patient_name_json)
+                # name=patient_name_json['given']
+                # print(name)
+                patient_name_data = json.loads(patient_name_json)
+                print(patient_name_json)
+                patients.append(patient_name_data[0][0]['given'])
+    except Exception as e:
+        print("Error retrieving patient names:", e)
+    finally:
+        if connection:
+            if patients:
+                connection.close_connection()
+                return patients
+            connection.close_connection()
+
 
 
 """
