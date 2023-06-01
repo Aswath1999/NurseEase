aabf5763845cddc97617f89ea7921494e8855456
Aswath1999 <csk.aswath@gmail.com>
2023-06-01 16:24:34 +0200

    added template create patient

--- routes/patientpostroutes.py
+++ routes/patientpostroutes.py
@@ -14,10 +14,10 @@ from .authentication import  is_logged_in
 templates = Jinja2Templates(directory="templates")
 
 patient= APIRouter()
-
+"""
 #post route for creating a new Patient
 @patient.post("/fhir/patient")
-async def create_patient(patient: Patient, request: Request,db_manager: DatabaseManager = Depends(database_connection)):
+async def create_patient(patient: Patient, request: Request,session: Session = Depends(database_connection)):
     try:
         primary_key_uuid = str(uuid4())
         patient.id = primary_key_uuid
@@ -25,18 +25,78 @@ async def create_patient(patient: Patient, request: Request,db_manager: Database
         # Create a new instance of the Patient model
         patient_model = pat(id=primary_key_uuid, patient=patient_json)
         # Add the patiet model to the session
-        db_manager.session.add(patient_model)
+        session.add(patient_model)
         # Commit the changes to the database
-        db_manager.session.commit()
+        session.commit()
         print("success")
     except Exception as e:
         print(e)
         return e  
-    finally:
-        if db_manager:                   # Close the session and connection
-            db_manager.close_connection()
-    
+""" 
 #getting names of patients
+@patient.post("/fhir/patient")
+async def create_patient(request: Request, session: Session = Depends(database_connection)):
+    try:
+        form = await request.form()
+        identifier_values = [form.get("identifier1"), form.get("identifier2")]
+        given_name = form.get("given")
+        family_name = form.get("family")
+        phone_number = form.get("telecom")
+        gender = form.get("gender")
+        birth_date = form.get("birthDate")
+        city = form.get("city")
+        state = form.get("state")
+        country = form.get("country")
+        
+        patient = Patient(
+            identifier=[
+                {"value": identifier_values[0]},
+                {"value": identifier_values[1]}
+            ],
+            name=[
+                {
+                    "given": [given_name],
+                    "family": family_name
+                }
+            ],
+            telecom=[
+                {
+                    "system": "phone",
+                    "value": phone_number
+                }
+            ],
+            gender=gender,
+            birthDate=birth_date,
+            address=[
+                {
+                    "city": city,
+                    "state": state,
+                    "country": country
+                }
+            ]
+        )
+        
+        primary_key_uuid = str(uuid4())
+        patient.id = primary_key_uuid
+        
+        patient_json = json.dumps(patient.dict(by_alias=True), cls=DateEncoder)
+        patient_model = pat(id=primary_key_uuid, patient=patient_json)
+        session.add(patient_model)
+        session.commit()
+        print("sucess")
+        return templates.TemplateResponse("success.html", {"request": request})
+    
+    except Exception as e:
+        print(e)
+        return e
+
+@patient.get("/createpatient")
+async def create_patient(request: Request):
+    try:
+       return templates.TemplateResponse("patients/createpatient.html", {"request": request})
+    except Exception as e:
+        print(e)
+
 
 @patient.get("/fhir/patient")
 @is_logged_in
