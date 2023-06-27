34aa81617b7ffb4f05744b4d370b6edd6f68e5a3
Aswath1999 <csk.aswath@gmail.com>
2023-06-27 16:20:55 +0200

    three working graphs

--- static/script.js
+++ static/script.js
@@ -202,7 +202,7 @@ function updateCharts(
   
     const chartContainers = document.querySelectorAll(".chart-container");
     chartContainers.forEach((container, index) => {
-      const chartType = ["o2", "o2_today", "hr", "hr_today", "temp", "temp_today"][index];
+      const chartType = ["o2_today","o2","hr_today",  "hr",  "temp_today", "temp"][index];
   
       if (chartType === "o2_today") {
         // Update the "o2_today" chart using Plotly.extendTraces or Plotly.update
@@ -435,48 +435,3 @@ function sendAndRetrieveObservationData() {
   getObservationData(); // Call the function to retrieve observation data
 }
 
-// Call the sendAndRetrieveObservationData function initially
-
-// Example usage: Trigger the updateCharts() function when the container is loaded
-
-// Helper function to group the data by day
-// function groupByDay(labels, data) {
-//     const groupedData = {};
-//     for (let i = 0; i < labels.length; i++) {
-//         const label = labels[i];
-//         const value = data[i];
-//         const day = label.substring(0, 10); // Extract the date part from the timestamp
-//         if (!groupedData[day]) {
-//             groupedData[day] = [];
-//         }
-//         groupedData[day].push(value);
-//     }
-//     return groupedData;
-// }
-
-// // Helper function to calculate the average of an array of numbers
-// function calculateAverage(numbers) {
-//     const sum = numbers.reduce((a, b) => a + b, 0);
-//     return sum / numbers.length;
-// }
-
-// Calculate the overall trend by taking the average of vital signs for each day
-// const overallLabels = [];
-// const overallO2LevelsAvg = [];
-// const overallHeartRatesAvg = [];
-
-// // Group the vital signs by day
-// const groupedByDayO2 = groupByDay(dayLabels, dayO2Levels);
-// const groupedByDayHR = groupByDay(dayLabels, dayHeartRates);
-
-// // Calculate the average oxygen level and heart rate for each day
-// for (const [date, o2Levels] of Object.entries(groupedByDayO2)) {
-//     const avgO2Level = calculateAverage(o2Levels);
-//     overallLabels.push(date);
-//     overallO2LevelsAvg.push(avgO2Level);
-// }
-
-// for (const [date, heartRates] of Object.entries(groupedByDayHR)) {
-//     const avgHeartRate = calculateAverage(heartRates);
-//     overallHeartRatesAvg.push(avgHeartRate);
-// }
